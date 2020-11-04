dca68fac0ced9c906a03ca886c9e3faa9687f75b
Nilton Junior <niltongouveajr@gmail.com>
2020-11-04 20:20:58 -0300

    Terraform early studies

--- terraform/infrastructure/security/main.tf
+++
@@ -1,8 +0,0 @@
-module "module-infrastructure-network-vpc" {
-  source = "../network/vpc"
-}
-
-module "module-infrastructure-security-sg" {
-  source = "./security-group"
-  vpc_id = module.module-infrastructure-network-vpc.vpc_id
-}
